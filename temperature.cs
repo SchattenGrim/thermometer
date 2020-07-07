96bf250c3458c3df2bc32a8b45e8359bda73115d
SchattenGrim <usanov.valya@mail.ru>
2020-07-07 18:53:12 +0600

    sum of numbers entered

---
+++ temperature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace thermometer
+{
+    internal class temperature<T>
+    {
+        public temperature()
+        {
+        }
+
+        internal void Add(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
